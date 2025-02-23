e3f8b61f56d16e667235144bbad3bb952222b6e2
Rafael Molina García <125874102+rafmolgar2@users.noreply.github.com>
2025-02-23 11:30:00 +0100

    Add backend inizialization

---
+++ backend/gastrostock/src/main/java/ispp_g2/gastrostock/GastrostockApplication.java
@@ -0,0 +1,13 @@
+package ispp_g2.gastrostock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastrostockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastrostockApplication.class, args);
+	}
+
+}
