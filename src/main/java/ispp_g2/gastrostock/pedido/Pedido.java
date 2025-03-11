8b95d3ca6ef0aaa986346682443fefe2071375b8
IbaiPerez <ibaperfer@alum.us.es>
2025-03-11 19:16:01 +0100

    feat/Relaciones de pedido añadidas

--- src/main/java/ispp_g2/gastrostock/pedido/Pedido.java
+++ src/main/java/ispp_g2/gastrostock/pedido/Pedido.java
@@ -2,8 +2,10 @@ package ispp_g2.gastrostock.pedido;
 
 import java.time.LocalDateTime;
 
+import ispp_g2.gastrostock.empleado.Empleado;
 import ispp_g2.gastrostock.mesa.Mesa;
 import ispp_g2.gastrostock.model.BaseEntity;
+import ispp_g2.gastrostock.negocio.Negocio;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -30,4 +32,11 @@ public class Pedido extends BaseEntity{
     @JoinColumn(name = "id")
     private Mesa mesa;
     
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "id")
+    private Empleado empleado;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "id")
+    private Negocio negocio;
 }
