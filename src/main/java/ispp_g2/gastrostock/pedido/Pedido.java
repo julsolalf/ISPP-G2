53efd2c91edeb6c0bd45c731752741113afea907
IbaiPerez <ibaperfer@alum.us.es>
2025-03-11 18:55:41 +0100

    feat/Entidad pedido y linea pedido creadas

--- src/main/java/ispp_g2/gastrostock/pedido/Pedido.java
+++ src/main/java/ispp_g2/gastrostock/pedido/Pedido.java
@@ -1,12 +1,33 @@
 package ispp_g2.gastrostock.pedido;
 
+import java.time.LocalDateTime;
+
+import ispp_g2.gastrostock.mesa.Mesa;
+import ispp_g2.gastrostock.model.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class Pedido {
+public class Pedido extends BaseEntity{
+
+    @NotNull
+    @PastOrPresent
+    private LocalDateTime fecha;
+
+    @NotNull
+    @Positive
+    private Double precioTotal;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "id")
+    private Mesa mesa;
     
 }
