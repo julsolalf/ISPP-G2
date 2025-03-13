b91b62fdda78901eda48a28befc988dd27317c06
José Miguel <josemic@protonmail.com>
2025-03-13 17:58:22 +0100

    feat(#98): agregar entidad ProductoVenta

--- src/main/java/ispp_g2/gastrostock/productoVenta/CategoriasVenta.java
+++ src/main/java/ispp_g2/gastrostock/productoVenta/CategoriasVenta.java
@@ -1,5 +1,7 @@
 package ispp_g2.gastrostock.productoVenta;
 
 public enum CategoriasVenta {
-    
+    BEBIDAS,
+    PLATOS,
+    POSTRES
 }
--- src/main/java/ispp_g2/gastrostock/productoVenta/ProductoVenta.java
+++ src/main/java/ispp_g2/gastrostock/productoVenta/ProductoVenta.java
@@ -1,14 +1,30 @@
 package ispp_g2.gastrostock.productoVenta;
 
+import ispp_g2.gastrostock.lineaDePedido.LineaDePedido;
 import ispp_g2.gastrostock.model.BaseEntity;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-// TEMPORAL FIX
 public class ProductoVenta extends BaseEntity {
-    
+
+    @NotBlank
+    private String nombre;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private CategoriasVenta categoriaVenta;
+
+    @NotNull
+    private Double precioVenta;
+
+    @ManyToOne
+    @JoinColumn(name = "linea_pedido_id")
+    private LineaDePedido lineaDePedido;
+
 }
