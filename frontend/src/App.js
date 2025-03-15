73dde00cb3cc89b6d67bca8c7eaa586389bf58b1
davvicval <davvicval@alum.us.es>
2025-03-15 12:02:40 +0100

    (feat): Cambio en el menú principal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <TPV />
+        <div className="logo-container">
+        <img src="/gastrostockLogo.png" alt="App Logo" className="app-logo" />
+        </div>
+        <div className="button-container">
+          <button onClick={() => console.log("Ir a Iniciar Sesión")}>
+            Iniciar Sesión
+          </button>
+          <button onClick={() => console.log("Ir a Registrarse")}>
+            Registrarse
+          </button>
+          <button onClick={() => console.log("Mostrar Más Información")}>
+            Más Información
+          </button>
+        </div>
       </header>
     </div>
   );
