2300ba20c125c2aafc437611ef1911d8e5d5655f
Rafael Molina García <rafmolgar2@alum.us.es>
2025-03-17 13:19:32 +0100

    feat: Configuración de seguridad añadida
    Se han modifica los parametros de application para tener todos el mismo auth a la hora de realizar peticiones.
    Tambien se ha añadido un fichero de seguridad para permitir configurar las uris

---
+++ src/main/java/ispp_g2/gastrostock/config/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package ispp_g2.gastrostock.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/lineasDePedido/**").permitAll()
+                        .anyRequest().authenticated());
+
+        return http.build();
+    }
+}
