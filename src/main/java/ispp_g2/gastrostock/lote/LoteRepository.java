9d8fa9f6f5ee5209df0a5eceb6881fe5f4046393
Rafael Molina García <rafmolgar2@alum.us.es>
2025-03-18 18:55:50 +0100

    feat: Controladores Lote Se han añadido tanto repositorio, servicio y controlador a la entidad Lote, su url esta tambien en el archivo de SecurityConfiguration

--- src/main/java/ispp_g2/gastrostock/lote/LoteRepository.java
+++ src/main/java/ispp_g2/gastrostock/lote/LoteRepository.java
@@ -1,7 +1,24 @@
 package ispp_g2.gastrostock.lote;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface LoteRepository extends CrudRepository<Lote,String> {
-    
+
+    @Query("SELECT l FROM Lote l WHERE l.cantidad = ?1")
+    List<Lote> findByCantidad(Integer cantidad);
+
+    @Query("SELECT l FROM Lote l WHERE l.fechaCaducidad = ?1")
+    List<Lote> findByFechaCaducidad(String fechaCaducidad);
+
+    @Query("SELECT l FROM Lote l WHERE l.producto.id = ?1")
+    List<Lote> findByProductoId(Integer producto);
+
+    @Query("SELECT l FROM Lote l WHERE l.reabastecimiento.id = ?1")
+    List<Lote> findByReabastecimientoId(Integer reabastecimiento);
+
 }
