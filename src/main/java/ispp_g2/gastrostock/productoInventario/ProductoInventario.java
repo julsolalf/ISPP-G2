ad29c6b6fba12a1c74424c46f78b5c9028fc0599
IbaiPerez <ibaperfer@alum.us.es>
2025-03-19 20:49:21 +0100

    feat(#125): Añadido metodo para calcular la cantidad del ProductoInventario

--- src/main/java/ispp_g2/gastrostock/productoInventario/ProductoInventario.java
+++ src/main/java/ispp_g2/gastrostock/productoInventario/ProductoInventario.java
@@ -1,5 +1,10 @@
 package ispp_g2.gastrostock.productoInventario;
 
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import ispp_g2.gastrostock.lote.Lote;
 import ispp_g2.gastrostock.model.NamedEntity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
@@ -23,5 +28,10 @@ public class ProductoInventario extends NamedEntity {
 
     @NotNull
     private Integer cantidadAviso;
+
+    @Transient
+    public Integer calcularCantidad(List<Lote> lotes) {
+        return lotes.stream().collect(Collectors.summingInt(Lote::getCantidad));
+    }
     
 }
