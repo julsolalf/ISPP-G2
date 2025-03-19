bacdb10777aba72e8a27f5bccf4f831cebc8ec73
alevarmun1 <alevarmun1@alum.us.es>
2025-03-19 13:01:38 +0100

    feat(#119): Add DiaReparto entity

---
+++ src/main/java/ispp_g2/gastrostock/diaReparto/DiaReparto.java
@@ -0,0 +1,33 @@
+package ispp_g2.gastrostock.diaReparto;
+
+import ispp_g2.gastrostock.model.BaseEntity;
+import ispp_g2.gastrostock.negocio.Negocio;
+
+import ispp_g2.gastrostock.proveedores.Proveedor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class DiaReparto extends BaseEntity {
+
+    @NotNull
+    private DiaSemana diaSemana;
+
+    @NotBlank
+    private String descripcion;
+
+    @ManyToOne
+    @NotNull
+    private Negocio negocio;
+
+    @ManyToOne
+    @NotNull
+    private Proveedor proveedor;
+    
+}
---
+++ src/main/java/ispp_g2/gastrostock/diaReparto/DiaSemana.java
@@ -0,0 +1,6 @@
+package ispp_g2.gastrostock.diaReparto;
+
+public enum DiaSemana {
+    LUNES, MARTES, MIERCOLES, JUEVES, VIERNES, SABADO, DOMINGO
+    
+}
