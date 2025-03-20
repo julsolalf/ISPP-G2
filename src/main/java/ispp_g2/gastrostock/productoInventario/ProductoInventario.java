c89316e52f7732c37310d07e79a9339e05f67b23
José Miguel <josemic@protonmail.com>
2025-03-20 17:51:58 +0100

    feat(#126): implementación de controlador, servicios y repositorios de user y authorities

--- src/main/java/ispp_g2/gastrostock/productoInventario/ProductoInventario.java
+++ src/main/java/ispp_g2/gastrostock/productoInventario/ProductoInventario.java
@@ -1,7 +1,6 @@
 package ispp_g2.gastrostock.productoInventario;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import ispp_g2.gastrostock.lote.Lote;
