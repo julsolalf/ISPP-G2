4b6a9844cebefdeb2875ab356c0d27a738480722
Rafael Molina García <rafmolgar2@alum.us.es>
2025-03-24 00:37:57 +0100

    fix: Arreglos en los controller
    Se han arreglado los controladores de la entidad Negocio

--- src/main/java/ispp_g2/gastrostock/negocio/NegocioRepository.java
+++ src/main/java/ispp_g2/gastrostock/negocio/NegocioRepository.java
@@ -2,26 +2,32 @@ package ispp_g2.gastrostock.negocio;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NegocioRepository extends CrudRepository<Negocio, String> {
 
-    public List<Negocio> findAll();
+    @Query("SELECT n FROM Negocio n WHERE n.name = ?1")
+    public Negocio findByName(String nombre);
 
-    public Negocio findById(Integer id);
+    @Query("SELECT n FROM Negocio n WHERE n.direccion = ?1")
+    public List<Negocio> findByDireccion(String direccion);
 
-    public Negocio findByDireccion(String direccion);
+    @Query("SELECT n FROM Negocio n WHERE n.tokenNegocio = ?1")
+    public Negocio findByTokenNegocio(Integer tokenNegocio);
 
-    public Negocio findByName(String name);
-
-    public List<Negocio> findByCiudad(String ciudad);
-
+    @Query("SELECT n FROM Negocio n WHERE n.codigoPostal = ?1")
     public List<Negocio> findByCodigoPostal(String codigoPostal);
 
+    @Query("SELECT n FROM Negocio n WHERE n.ciudad = ?1")
+    public List<Negocio> findByCiudad(String ciudad);
+
+    @Query("SELECT n FROM Negocio n WHERE n.pais = ?1")
     public List<Negocio> findByPais(String pais);
 
-    public Negocio findByTokenNegocio(Integer token);
+    @Query("SELECT n FROM Negocio n WHERE n.dueño.id = ?1")
+    public List<Negocio> findByDueño(String dueño);
 
 }
--- src/main/java/ispp_g2/gastrostock/negocio/NegocioService.java
+++ src/main/java/ispp_g2/gastrostock/negocio/NegocioService.java
@@ -3,71 +3,72 @@ package ispp_g2.gastrostock.negocio;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import jakarta.validation.Valid;
 
 import java.util.List;
+import java.util.stream.StreamSupport;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
 public class NegocioService {
 
-    private NegocioRepository nr;
+    private final NegocioRepository negocioRepository;
 
     @Autowired
     public NegocioService(NegocioRepository negocioRepository) {
-        this.nr = negocioRepository;
+        this.negocioRepository = negocioRepository;
     }
 
     @Transactional(readOnly = true)
-    public Negocio getById(Integer id) {
-        return nr.findById(id);
+    public Negocio getById(String id) {
+        return negocioRepository.findById(id).orElse(null);
     }
 
     @Transactional(readOnly = true)
     public List<Negocio> getNegocios() {
-        return nr.findAll();
+        Iterable<Negocio> negocios = negocioRepository.findAll();
+        return StreamSupport.stream(negocios.spliterator(), false)
+                .toList();
     }
 
     @Transactional(readOnly = true)
     public Negocio getByName(String name) {
-        return nr.findByName(name);
+        return negocioRepository.findByName(name);
     }
 
     @Transactional(readOnly = true)
-    public Negocio getByDireccion(String direccion) {
-        return nr.findByDireccion(direccion);
+    public List<Negocio> getByDireccion(String direccion) {
+        return negocioRepository.findByDireccion(direccion);
     }
 
     @Transactional(readOnly = true)
     public Negocio getByToken(Integer token) {
-        return nr.findByTokenNegocio(token);
+        return negocioRepository.findByTokenNegocio(token);
     }
 
     @Transactional(readOnly = true)
     public List<Negocio> getByCiudad(String ciudad) {
-        return nr.findByCiudad(ciudad);
+        return negocioRepository.findByCiudad(ciudad);
     }
 
     @Transactional(readOnly = true)
     public List<Negocio> getByPais(String pais) {
-        return nr.findByPais(pais);
+        return negocioRepository.findByPais(pais);
     }
 
     @Transactional(readOnly = true)
     public List<Negocio> getByCodigoPostal(String codigoPostal) {
-        return nr.findByCodigoPostal(codigoPostal);
+        return negocioRepository.findByCodigoPostal(codigoPostal);
     }
 
     @Transactional
-    public Negocio saveNegocio(@Valid Negocio newNegocio){
-        return nr.save(newNegocio);
+    public Negocio save(Negocio newNegocio){
+        return negocioRepository.save(newNegocio);
     }
 
     @Transactional
-    public void deleteNegocioByToken(Integer token) {
-        Negocio n = nr.findByTokenNegocio(token);
-        nr.delete(n);
+    public void delete(String id){
+        negocioRepository.deleteById(id);
     }
     
 }
