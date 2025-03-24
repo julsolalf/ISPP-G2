5b22b02ee2c2dacf1e3acbf1d3ad8514bf65058d
julsolalf <juliosolisalfonso@gmail.com>
2025-03-24 18:33:58 +0100

    fix(#194): Solucionar bucles jsons.

--- src/main/java/ispp_g2/gastrostock/lote/Lote.java
+++ src/main/java/ispp_g2/gastrostock/lote/Lote.java
@@ -13,6 +13,10 @@ import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 @Entity
 @Getter
 @Setter
