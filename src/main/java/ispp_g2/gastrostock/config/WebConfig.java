7ec3dbb8b93ea03f37b30bafa035dfa7b68d551e
davvicval <davvicval@alum.us.es>
2025-03-25 23:43:36 +0100

    feat(#225): Ver listado de ventas y ver venta en especifico(está conectada a backend)

---
+++ src/main/java/ispp_g2/gastrostock/config/WebConfig.java
@@ -0,0 +1,18 @@
+package ispp_g2.gastrostock.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // Permitir CORS para todos los endpoints
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")  // Permitir solicitudes desde el frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE")  // Métodos permitidos
+                .allowedHeaders("*");  // Permitir todos los encabezados
+    }
+}
