b91ac539f6ebaa361b090a9252ce6e57d8c4688a
julsolalf <juliosolisalfonso@gmail.com>
2025-04-09 01:20:18 +0200

    feat(#170): Login funciona, falta deserealizar bien checkout.session.completed

--- src/main/java/ispp_g2/gastrostock/config/SecurityConfiguration.java
+++ src/main/java/ispp_g2/gastrostock/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package ispp_g2.gastrostock.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -30,10 +31,11 @@ public class SecurityConfiguration {
 
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http
+        http    
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                        .requestMatchers("/api/subscriptions/webhook").permitAll() // Permitir webhooks sin autenticación
+                        .requestMatchers("/api/subscriptions/webhook").permitAll() // Permitir webhooks sin autenticación // Permitir acceso a la API de suscripciones sin autenticació
                         .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(AbstractHttpConfigurer::disable)
--- src/main/java/ispp_g2/gastrostock/config/jwt/JwtAuthFilter.java
+++ src/main/java/ispp_g2/gastrostock/config/jwt/JwtAuthFilter.java
@@ -27,9 +27,17 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            filterChain.doFilter(request, response);
+            return;
+        }
         try {
             String jwt = getJwt(request);
+            // Se verifica que se haya enviado un token
+            if (!StringUtils.hasText(jwt)) {
+                filterChain.doFilter(request, response);
+                return;
+            }
             String username;
 
             username=jwtService.getUserNameFromJwtToken(jwt);
