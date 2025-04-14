108869635f4efcbdeb0ba22a0c3eb457f9d94ebc
Raúl Toro Romero <toro.romero.raul@gmail.com>
2025-04-14 18:36:58 +0200

    feat(frontend): improved registroNegocio screen

--- frontend/src/screens/registroNegocio/index.js
+++ frontend/src/screens/registroNegocio/index.js
@@ -67,62 +67,65 @@ function PantallaRegistroNegocio() {
   };
 
   return (
-    <div className="content">
-      <div className="icon-container-right">
-        <Bell size={30} className="icon" onClick={toggleNotifications} />
-        <User size={30} className="icon" onClick={toggleUserOptions} />
+  <div className="page-container">
+    <div className="fondo">
+      <div>
+        <button onClick={() => navigate("/")} className="back-button" style={{marginLeft:"15%", fontSize:"28px"}}>⬅ Volver</button>
+        <Bell size={40} style={{marginLeft:"80%",marginTop:"1%"}}className="icon" onClick={toggleNotifications} />
+        <User size={40} style={{marginLeft:"3%"}} className="icon" onClick={toggleUserOptions} />
       </div>
-
       {showNotifications && (
-        <div className="notification-bubble">
-          <div className="notification-header">
-            <strong>Notificaciones</strong>
-            <button className="close-btn" onClick={toggleNotifications}>X</button>
-          </div>
-          <ul>
-            <li>Notificación 1</li>
-            <li>Notificación 2</li>
-            <li>Notificación 3</li>
-          </ul>
+      <div className="notification-bubble" style={{marginRight:"17.5%",marginTop:"5%"}}>
+        <div className="notification-header">
+          <strong>Notificaciones</strong>
+          <button className="close-btn" onClick={toggleNotifications}>X</button>
         </div>
-      )}
-
+        <ul>
+          <li>Notificación 1</li>
+          <li>Notificación 2</li>
+          <li>Notificación 3</li>
+        </ul>
+      </div>)}
       {showUserOptions && (
-        <div className="notification-bubble user-options">
+        <div className="notification-bubble user-options" style={{marginRight:"13.5%",marginTop:"5%"}}>
           <div className="notification-header">
             <strong>Usuario</strong>
             <button className="close-btn" onClick={toggleUserOptions}>X</button>
           </div>
           <ul>
-            <li>
-              <button className="user-btn" onClick={() => navigate("/perfil")}>Ver Perfil</button>
-            </li>
-            <li>
-              <button className="user-btn" onClick={() => navigate("/planes")}>Ver planes</button>
-            </li>
-            <li>
-              <button className="user-btn logout-btn" onClick={() => setShowLogoutModal(true)}>Cerrar Sesión</button>
-            </li>
+            <li><button className="user-btn" onClick={() => navigate("/perfil")}>Ver Perfil</button></li>
+            <li><button className="user-btn" onClick={() => navigate("/planes")}>Ver planes</button></li>
+            <button className="user-btn logout-btn" onClick={() => setShowLogoutModal(true)}>Cerrar Sesión</button>
           </ul>
-        </div>
-      )}
-
-      <button className="back-button" onClick={() => navigate(-1)}>← Volver</button>
-
+        </div>)}
       <img src="/gastrostockLogoSinLetra.png" alt="App Logo" className="app-logo" />
-      <h1 className="title">GastroStock</h1>
+      <div className="clearfix"/>
+      <h1 className="title" style={{marginTop:"5%"}}>GastroStock</h1>
+      <div className="clearfix"/>
       <h2>Registrar negocio</h2>
-
-      <input type="text" placeholder="Nombre del negocio" value={name} onChange={(e) => setName(e.target.value)} />
-      <input type="text" placeholder="Token de negocio" value={tokenNegocio} onChange={(e) => setTokenNegocio(e.target.value)} />
-      <input type="text" placeholder="Dirección" value={direccion} onChange={(e) => setDireccion(e.target.value)} />
-      <input type="text" placeholder="Código Postal" value={codigoPostal} onChange={(e) => setCodigoPostal(e.target.value)} />
-      <input type="text" placeholder="Ciudad" value={ciudad} onChange={(e) => setCiudad(e.target.value)} />
-      <input type="text" placeholder="País" value={pais} onChange={(e) => setPais(e.target.value)} />
-
-      <button onClick={handleRegister} className="login-btn" disabled={loading}>{loading ? "Registrando..." : "Registrar negocio"}</button>
-      <button  className="login-btn" onClick={() => navigate("/elegirNegocio")}>Registrar negocio más tarde</button>
-
+      <div style={{textAlign :"left", width:"80%", marginLeft:"10%"}}>
+        <strong style={{fontSize:"22px"}}>Nombre del negocio:</strong>
+        <div className="clearfix"/>
+        <input type="text" placeholder="Nombre del negocio" value={name} onChange={(e) => setName(e.target.value)} />
+        <div className="clearfix" style={{marginTop:"1%"}}/>
+        <strong style={{fontSize:"22px"}}>Token de negocio:</strong>
+        <div className="clearfix"/>
+        <input type="text" placeholder="Token de negocio" value={tokenNegocio} onChange={(e) => setTokenNegocio(e.target.value)} />
+        <div className="clearfix" style={{marginTop:"1%"}}/>
+        <strong style={{fontSize:"22px"}}>País:</strong>
+        <strong style={{fontSize:"22px", marginLeft:"33%"}}>Ciudad:</strong>
+        <strong style={{fontSize:"22px", marginLeft:"38%"}}>Código Postal:</strong>
+        <div className="clearfix"/>
+        <input type="text" style={{width:"33%"}} placeholder="País" value={pais} onChange={(e) => setPais(e.target.value)} />
+        <input type="text" style={{width:"40%", marginLeft:"3%"}} placeholder="Ciudad" value={ciudad} onChange={(e) => setCiudad(e.target.value)} />
+        <input type="text" style={{width:"15%", marginLeft:"3%"}} placeholder="Código Postal" value={codigoPostal} onChange={(e) => setCodigoPostal(e.target.value)} />
+        <div className="clearfix" style={{marginTop:"1%"}}/>
+        <strong style={{fontSize:"22px"}}>Dirección:</strong>
+        <div className="clearfix"/>
+        <input type="text" placeholder="Dirección" value={direccion} onChange={(e) => setDireccion(e.target.value)} />
+      </div>
+      <div className="clearfix" style={{marginTop:"1%"}}/>
+      <button onClick={handleRegister} className="login-btn" disabled={loading} style={{fontSize:"26px"}}>{loading ? "Registrando..." : "Registrar negocio"}</button>
       {/* Modal de Confirmación para Logout */}
       {showLogoutModal && (
         <div className="modal-overlay">
@@ -134,8 +137,9 @@ function PantallaRegistroNegocio() {
             </div>
           </div>
         </div>
-      )}
+        )}
     </div>
+  </div>
   );
 }
 
