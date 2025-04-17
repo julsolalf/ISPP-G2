6adbd2719b8ce30a28de93c834595fb69e50b549
IbaiPerez <ibaperfer@alum.us.es>
2025-04-17 21:12:03 +0200

    feat/ Restricciones CRUD de categorias creadas

--- src/main/java/ispp_g2/gastrostock/categorias/CategoriaRepository.java
+++ src/main/java/ispp_g2/gastrostock/categorias/CategoriaRepository.java
@@ -15,4 +15,10 @@ public interface CategoriaRepository extends CrudRepository<Categoria, Integer> 
     @Query("SELECT c FROM Categoria c WHERE c.negocio.id = ?1")
     List<Categoria> findByNegocioId(Integer negocioId);
 
+    @Query("SELECT c FROM Categoria c WHERE c.negocio.id =?1 AND c.pertenece = 0")
+    List<Categoria> findInventarioByNegocioId(Integer negocioId);
+
+    @Query("SELECT c FROM Categoria c WHERE c.negocio.id =?1 AND c.pertenece = 1")
+    List<Categoria> findVentaByNegocioId(Integer negocioId);
+
 }
--- src/main/java/ispp_g2/gastrostock/categorias/CategoriaService.java
+++ src/main/java/ispp_g2/gastrostock/categorias/CategoriaService.java
@@ -1,7 +1,13 @@
 package ispp_g2.gastrostock.categorias;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ispp_g2.gastrostock.exceptions.ResourceNotFoundException;
+import ispp_g2.gastrostock.negocio.NegocioRepository;
+import jakarta.validation.Valid;
 
 import java.util.List;
 import java.util.stream.StreamSupport;
@@ -10,36 +16,69 @@ import java.util.stream.StreamSupport;
 public class CategoriaService {
 
     private final CategoriaRepository categoriaRepository;
+    private final NegocioRepository negocioRepository;
 
     @Autowired
-    public CategoriaService(CategoriaRepository categoriaRepository) {
+    public CategoriaService(CategoriaRepository categoriaRepository, NegocioRepository negocioRepository) {
         this.categoriaRepository = categoriaRepository;
+        this.negocioRepository = negocioRepository;
     }
 
+    @Transactional(readOnly = true)
     public Categoria getById(Integer id) {
-        return categoriaRepository.findById(id).orElse(null);
+        return categoriaRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("La categoria no existe"));
     }
 
+    @Transactional(readOnly = true)
     public List<Categoria> getCategorias() {
         Iterable<Categoria> categorias = categoriaRepository.findAll();
         return StreamSupport.stream(categorias.spliterator(), false)
                 .toList();
     }
 
+    @Transactional(readOnly = true)
     public List<Categoria> getCategoriasByNegocioId(Integer negocioId) {
         return categoriaRepository.findByNegocioId(negocioId);
     }
 
+    @Transactional(readOnly = true)
     public List<Categoria> getCategoriasByName(String name) {
         return categoriaRepository.findByName(name);
     }
 
+    @Transactional(readOnly = true)
+    public List<Categoria> getCategoriasInventarioByNegocioId(Integer negocioId) {
+        return categoriaRepository.findInventarioByNegocioId(negocioId);
+    }
 
+    @Transactional(readOnly = true)
+    public List<Categoria> getCategoriasVentaByNegocioId(Integer negocioId) {
+        return categoriaRepository.findVentaByNegocioId(negocioId);
+    }
+
+    @Transactional
     public Categoria save(Categoria categoria) {
         return categoriaRepository.save(categoria);
     }
 
+    @Transactional
+    public Categoria update(Integer id, Categoria categoria) {
+        Categoria toUpdate = categoriaRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("La categoria no existe"));
+        BeanUtils.copyProperties(categoria, toUpdate, "id");
+        return categoriaRepository.save(toUpdate);
+    }
+
+    @Transactional
     public void delete(Integer id) {
         categoriaRepository.deleteById(id);
+    }
+
+    public Categoria convertirCategoria(CategoriaDTO categoriaDTO) {
+        Categoria categoria = new Categoria();
+        categoria.setName(categoriaDTO.getNombre());
+        categoria.setNegocio(negocioRepository.findById(categoriaDTO.getNegocioId())
+            .orElseThrow(() -> new ResourceNotFoundException("El negocio no existe")));
+        categoria.setPertenece(categoriaDTO.getPertenece());
+        return categoria;
     }
 }
